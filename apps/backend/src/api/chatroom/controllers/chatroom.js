e60028948426ed1285f6fc3659c74fd4498eadd6
Sudeep kudari <sudeepkudari0@gmail.com>
2024-11-08 01:14:57 +0530

    project init

---
+++ apps/backend/src/api/chatroom/controllers/chatroom.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chatroom controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::chatroom.chatroom');
